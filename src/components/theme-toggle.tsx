629fcdf86c9198f7b2bb01d66e303f6b2a3d5153
jsuryakt-cosmo <jayasurya@cosmocp.com>
2025-12-13 09:30:46 +0530

    fix toggle

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,32 +1,45 @@
 "use client"
-
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
 
 export function ThemeToggle() {
-  const { resolvedTheme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+      <Button variant="ghost" size="icon" className="rounded-full" />
+    )
+  }
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
-      className="rounded-full relative"
+      onClick={() =>
+        setTheme(resolvedTheme === "light" ? "dark" : "light")
+      }
+      className="relative rounded-full"
     >
       <Sun
-        className={
-          `h-[1.2rem] w-[1.2rem] transition-all ${resolvedTheme === "dark" ? "-rotate-90 scale-0" : "rotate-0 scale-100"}`
-        }
+        className={`h-[1.2rem] w-[1.2rem] transition-all ${resolvedTheme === "dark"
+            ? "-rotate-90 scale-0"
+            : "rotate-0 scale-100"
+          }`}
       />
       <Moon
-        className={
-          `absolute h-[1.2rem] w-[1.2rem] transition-all ${resolvedTheme === "dark" ? "rotate-0 scale-100" : "rotate-90 scale-0"}`
-        }
+        className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${resolvedTheme === "dark"
+            ? "rotate-0 scale-100"
+            : "rotate-90 scale-0"
+          }`}
       />
       <span className="sr-only">Toggle theme</span>
     </Button>
-  );
+  )
 }
